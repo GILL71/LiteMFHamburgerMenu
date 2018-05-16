aa763b18bfb5345f71832246395d17a2d261b512
Michael Nechaev <gm360@mail.ru>
2018-05-16 11:31:18 +0300

    Initial Commit

---
+++ LiteMFHamburgerMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiteMFHamburgerMenu
+//
+//  Created by Michael Nechaev on 16/05/2018.
+//  Copyright © 2018 Michael Nechaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
